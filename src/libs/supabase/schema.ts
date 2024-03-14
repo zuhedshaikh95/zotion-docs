60be0fa6905d3392e39f8726987e9297aea55572
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-14 09:16:39 +0530

    add: Subscription

--- src/libs/supabase/schema.ts
+++ src/libs/supabase/schema.ts
@@ -1,4 +1,5 @@
-import { pgTable, uuid, timestamp, text } from "drizzle-orm/pg-core";
+import { pgTable, uuid, timestamp, text, jsonb, integer, boolean } from "drizzle-orm/pg-core";
+import { prices, subscriptionStatus, users } from "../../../migrations/schema";
 
 export const Workspaces = pgTable("workspaces", {
   id: uuid("id").defaultRandom().primaryKey().notNull(),
@@ -35,4 +36,24 @@ export const Files = pgTable("files", {
   logo: text("logo"),
   bannerUrl: text("banner_url"),
   createdAt: timestamp("created_at", { mode: "string", withTimezone: true }).defaultNow(),
+});
+
+export const Subscriptions = pgTable("subscriptions", {
+  id: text("id").primaryKey().notNull(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id),
+  status: subscriptionStatus("status"),
+  metadata: jsonb("metadata"),
+  priceId: text("price_id").references(() => prices.id),
+  quantity: integer("quantity"),
+  cancelAtPeriodEnd: boolean("cancel_at_period_end"),
+  created: timestamp("created", { withTimezone: true, mode: "string" }).defaultNow().notNull(),
+  currentPeriodStart: timestamp("current_period_start", { withTimezone: true, mode: "string" }).defaultNow().notNull(),
+  currentPeriodEnd: timestamp("current_period_end", { withTimezone: true, mode: "string" }).defaultNow().notNull(),
+  endedAt: timestamp("ended_at", { withTimezone: true, mode: "string" }).defaultNow(),
+  cancelAt: timestamp("cancel_at", { withTimezone: true, mode: "string" }).defaultNow(),
+  canceledAt: timestamp("canceled_at", { withTimezone: true, mode: "string" }).defaultNow(),
+  trialStart: timestamp("trial_start", { withTimezone: true, mode: "string" }).defaultNow(),
+  trialEnd: timestamp("trial_end", { withTimezone: true, mode: "string" }).defaultNow(),
 });
