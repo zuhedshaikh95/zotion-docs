9a85557af9a98787d1495a23aa32272bb26a90fd
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-16 23:27:40 +0530

    add: exports

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+// Index page
+export { default as TitleSection } from "./index-page/title-section";
+
+// ui
+export { default as Button } from "./ui/button";
+export { default as Container } from "./ui/container";
