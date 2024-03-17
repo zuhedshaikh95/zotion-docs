5d09154d7aace83e5e91eab0433929bdfc40855f
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-17 18:17:41 +0530

    fix: int price

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -107,14 +107,14 @@ export const USERS = [
 export const PRICING_CARDS = [
   {
     planType: "Free Plan",
-    price: "0",
+    price: 0,
     description: "Limited block trials  for teams",
     highlightFeature: "",
     freatures: ["Unlimited blocks for teams", "Unlimited file uploads", "30 day page history", "Invite 2 guests"],
   },
   {
     planType: "Pro Plan",
-    price: "12.99",
+    price: 12.99,
     description: "Billed annually. $17 billed monthly",
     highlightFeature: "Everything in free +",
     freatures: ["Unlimited blocks for teams", "Unlimited file uploads", "1 year day page history", "Invite 10 guests"],
