5baa5f46bf8cbdb4770cf00b67ec1350ba96a1b8
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-17 18:14:40 +0530

    add: export Card, Avatar primitives

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 // Index page
 export { default as TitleSection } from "./index-page/title-section";
+export { default as Card } from "./index-page/custom-card";
 
 // ui
 export { default as Button } from "./ui/button";
 export { default as Container } from "./ui/container";
+export * as Avatar from "./ui/avatar";
