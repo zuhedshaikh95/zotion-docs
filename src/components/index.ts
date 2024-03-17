518a1d376e7e33ae7562ce38ae7f9d444d17abd8
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-17 23:56:03 +0530

    add: export Form, Input

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,4 +5,7 @@ export { default as Card } from "./index-page/custom-card";
 // ui
 export { default as Button } from "./ui/button";
 export { default as Container } from "./ui/container";
+export { default as Input } from "./ui/input";
+
 export * as Avatar from "./ui/avatar";
+export * as Form from "./ui/form";
