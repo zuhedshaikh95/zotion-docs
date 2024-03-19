c06626d65bbbb076ea9237d888ae973917c55acc
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-19 22:49:25 +0530

    add: export NavigationMenu

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,16 @@
 // Index page
-export { default as TitleSection } from "./index-page/title-section";
-export { default as Card } from "./index-page/custom-card";
+export { default as TitleSection } from "./landing-page/title-section";
+export { default as Card } from "./landing-page/custom-card";
+export { default as Navbar } from "./landing-page/navbar";
 
 // ui
 export { default as Button } from "./ui/button";
 export { default as Container } from "./ui/container";
 export { default as Input } from "./ui/input";
 
+// global
+export { default as Loader } from "./global/Loader";
+
 export * as Avatar from "./ui/avatar";
 export * as Form from "./ui/form";
+export * as NavigationMenu from "./ui/navigation-menu";
