1bbfccc7f1a0c8895db21cb65acc7463769de6fd
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-20 23:26:53 +0530

    add: signupFormSchema

---
+++ src/libs/validations/signup.ts
@@ -0,0 +1,12 @@
+import * as z from "zod";
+
+export const signupFormSchema = z
+  .object({
+    email: z.string().describe("Email").email({ message: "Invalid Email" }),
+    password: z.string().describe("Password").min(6, "Password must be of minimum 6 characters"),
+    confirmPassword: z.string().describe("Confirm Password").min(6, "Password must be of minimum 6 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords did not match! Try again",
+    path: ["confirmPassword"],
+  });
