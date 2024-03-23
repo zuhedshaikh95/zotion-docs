86fbfbc9ed9ef4ee82c71c953e5ed442c97d2d3e
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-23 22:04:40 +0530

    fix: imports

--- src/libs/supabase/db.ts
+++ src/libs/supabase/db.ts
@@ -1,8 +1,8 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
 import * as dotenv from "dotenv";
-import * as schema from "../../../migrations/schema";
+import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
+import * as schema from "../../../migrations/schema";
 dotenv.config({ path: ".env" });
 
 if (!process.env.DATABASE_URL) {
@@ -15,9 +15,9 @@ const db = drizzle(client, { schema });
 
 (async () => {
   console.log("🟠 Migrating client!");
-  await migrate(db, { migrationsFolder: "migrations" });
-  console.log("🟢 Successfully migrated!");
   try {
+    await migrate(db, { migrationsFolder: "migrations" });
+    console.log("🟢 Successfully migrated!");
   } catch (error: any) {
     console.log("🔴 Migration Error:", error.message);
   }
