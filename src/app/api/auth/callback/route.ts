51e18adf1741a7a0c509bf2427e0b90bb7621776
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-25 16:36:27 +0530

    fix: client side error handling

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -7,21 +7,11 @@ export async function GET(request: NextRequest) {
     const requestUrl = new URL(request.url);
     const code = requestUrl.searchParams.get("code");
 
-    if (!code) {
-      return NextResponse.json(
-        {
-          message: "Bad Request",
-          error: true,
-        },
-        { status: 400 }
-      );
-    }
-
     const supabase = createRouteHandlerClient({ cookies });
     await supabase.auth.exchangeCodeForSession(code!);
 
     return NextResponse.redirect(`${requestUrl.origin}/dashboard`);
   } catch (error: any) {
-    return NextResponse.json({ message: error.message }, { status: 500 });
+    return NextResponse.json({ message: error.message, error: true, success: false }, { status: 500 });
   }
 }
