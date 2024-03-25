0041d451b4fec00b0fb9d9d9df272f1da7838e2b
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-25 16:37:19 +0530

    add: common export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,28 @@
-// Index page
+// landing
 export { default as TitleSection } from "./landing-page/title-section";
-export { default as Card } from "./landing-page/custom-card";
+export { default as CustomCard } from "./landing-page/custom-card";
 export { default as Navbar } from "./landing-page/navbar";
 
-// ui
-export { default as Button } from "./ui/button";
-export { default as Container } from "./global/container";
-export { default as Input } from "./ui/input";
+// dasboard
+export { default as DashboardSetup } from "./dashboard/dashboard-setup";
+
+// sidebar
+export { default as WorkspaceDropdown } from "./sidebar/WorkspaceDropdown";
 
 // global
 export { default as Loader } from "./global/Loader";
+export { default as Container } from "./global/container";
+export { default as EmojiPicker } from "./global/emoji-picker";
+export { default as Sidebar } from "./sidebar/sidebar";
 
+// ui
+export { default as Button } from "./ui/button";
+export { default as Input } from "./ui/input";
+export { Label } from "./ui/label";
+
+export * as Card from "./ui/card";
 export * as Avatar from "./ui/avatar";
 export * as Form from "./ui/form";
 export * as NavigationMenu from "./ui/navigation-menu";
 export * as Alert from "./ui/alert";
+export * as Popover from "./ui/popover";
