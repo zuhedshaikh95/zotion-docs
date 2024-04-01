908f79397c7c6201f610deb30fcf3d2c0177d1e1
zuhedshaikh95 <zuhed95@gmail.com>
2024-04-01 23:32:52 +0530

    add: export Tooltip, Accordian etc

--- src/components/index.ts
+++ src/components/index.ts
@@ -11,6 +11,8 @@ export { default as Sidebar } from "./sidebar/sidebar";
 export { default as WorkspaceDropdown } from "./sidebar/workspace-dropdown";
 export { default as SelectedWorkspace } from "./sidebar/selected-workspace";
 export { default as PlanUsage } from "./sidebar/plan-usage";
+export { default as NativeNavigation } from "./sidebar/native-navigation";
+export { default as FoldersDropdownList } from "./sidebar/folders-dropdown-list";
 
 // global
 export { default as Loader } from "./global/Loader";
@@ -19,6 +21,8 @@ export { default as EmojiPicker } from "./global/emoji-picker";
 export { default as CustomDialogTrigger } from "./global/custom-dialogue-trigger";
 export { default as WorkspaceCreator } from "./global/workspace-creator";
 export { default as CollaboratorSearch } from "./global/collaborators-search";
+export { default as CustomTooltip } from "./global/custom-tooltip";
+export { default as Dropdown } from "./sidebar/dropdown";
 
 // ui
 export { default as Button } from "./ui/button";
@@ -36,3 +40,5 @@ export * as Dialog from "./ui/dialog";
 export * as Select from "./ui/select";
 export * as Sheet from "./ui/sheet";
 export * as Scroll from "./ui/scroll-area";
+export * as Tooltip from "./ui/tooltip";
+export * as Accordian from "./ui/accordion";
