47a30c4d4d116b3bca5068716c5a5c2c5ba8b1a2
zuhedshaikh95 <zuhed95@gmail.com>
2024-04-02 23:46:13 +0530

    fix: hydration error

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -17,18 +17,18 @@ Item.displayName = "AccordionItem";
 const Trigger = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-  <AccordionPrimitive.Header className="flex">
+>(({ className, children, disabled, ...props }, ref) => (
+  <AccordionPrimitive.Header className="flex justify-between items-center">
+    {children}
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "flex items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
         className
       )}
       {...props}
     >
-      {children}
-      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+      {!disabled && <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />}
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ));
