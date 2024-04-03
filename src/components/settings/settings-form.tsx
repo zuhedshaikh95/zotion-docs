5d2e81da5a282dcc0294210ca38b7f610df6909e
zuhedshaikh95 <zuhed95@gmail.com>
2024-04-03 22:54:32 +0530

    add: SettingsForm

---
+++ src/components/settings/settings-form.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+interface Props {}
+
+const SettingsForm: React.FC<Props> = ({}) => {
+  return <div>SettingsForm</div>;
+};
+
+export default SettingsForm;
