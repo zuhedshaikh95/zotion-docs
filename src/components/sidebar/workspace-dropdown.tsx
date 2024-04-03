51f4507b2da8a70dfa80ba762f517ad48c7b7e08
zuhedshaikh95 <zuhed95@gmail.com>
2024-04-03 22:57:14 +0530

    fix: scrollbar styling

--- src/components/sidebar/workspace-dropdown.tsx
+++ src/components/sidebar/workspace-dropdown.tsx
@@ -3,7 +3,7 @@ import { useAppState } from "@/libs/providers/app-state-provider";
 import { WorkspaceI } from "@/libs/supabase/supabase.types";
 import { Plus } from "lucide-react";
 import React, { useEffect, useState } from "react";
-import { CustomDialogTrigger, SelectedWorkspace, WorkspaceCreator } from "..";
+import { CustomDialogTrigger, Scroll, SelectedWorkspace, WorkspaceCreator } from "..";
 
 interface Props {
   privateWorkspaces: WorkspaceI[];
@@ -60,49 +60,45 @@ const WorkspaceDropdown: React.FC<Props> = ({
             origin-top-right
             absolute
             w-full
-            overflow-y-auto
             rounded-md
             shadow-md
             z-50
-            max-h-[300px]
             bg-black/10
             backdrop-blur-lg
             group
             border-[1px]
             border-muted"
         >
-          <div className="rounded-md flex flex-col">
-            <div className="">
-              {!!privateWorkspaces.length && (
-                <>
-                  <p className="text-sm text-muted-foreground p-2">Private</p>
-                  {privateWorkspaces.map((option) => (
-                    <SelectedWorkspace key={option.id} workspace={option} onClick={handleSelect} />
-                  ))}
-                </>
-              )}
+          <Scroll.Area className="rounded-md flex flex-col max-h-[250px]">
+            {!!privateWorkspaces.length && (
+              <>
+                <p className="text-sm text-muted-foreground p-2">Private</p>
+                {privateWorkspaces.map((option) => (
+                  <SelectedWorkspace key={option.id} workspace={option} onClick={handleSelect} />
+                ))}
+              </>
+            )}
 
-              {!!sharedWorkspaces.length && (
-                <>
-                  <hr />
-                  <p className="text-sm text-muted-foreground p-2">Shared</p>
-                  {sharedWorkspaces.map((option) => (
-                    <SelectedWorkspace key={option.id} workspace={option} onClick={handleSelect} />
-                  ))}
-                </>
-              )}
+            {!!sharedWorkspaces.length && (
+              <>
+                <hr />
+                <p className="text-sm text-muted-foreground p-2">Shared</p>
+                {sharedWorkspaces.map((option) => (
+                  <SelectedWorkspace key={option.id} workspace={option} onClick={handleSelect} />
+                ))}
+              </>
+            )}
 
-              {!!collaboratingWorkspaces.length && (
-                <>
-                  <hr />
-                  <p className="text-sm text-muted-foreground p-2">Shared</p>
-                  {collaboratingWorkspaces.map((option) => (
-                    <SelectedWorkspace key={option.id} workspace={option} onClick={handleSelect} />
-                  ))}
-                </>
-              )}
-            </div>
-          </div>
+            {!!collaboratingWorkspaces.length && (
+              <>
+                <hr />
+                <p className="text-sm text-muted-foreground p-2">Shared</p>
+                {collaboratingWorkspaces.map((option) => (
+                  <SelectedWorkspace key={option.id} workspace={option} onClick={handleSelect} />
+                ))}
+              </>
+            )}
+          </Scroll.Area>
         </div>
       )}
       <CustomDialogTrigger
