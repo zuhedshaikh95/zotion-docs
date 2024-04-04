955ba9cf94fa691e13217479117da60c5a0e126e
zuhedshaikh95 <zuhed95@gmail.com>
2024-04-04 23:52:35 +0530

    add: SettingsForm

--- src/components/settings/settings-form.tsx
+++ src/components/settings/settings-form.tsx
@@ -1,9 +1,25 @@
 "use client";
-import React from "react";
+import React, { useRef, useState } from "react";
+import { useAppState } from "@/libs/providers/app-state-provider";
+import { useToast } from "../ui/use-toast";
+import { UserI } from "@/libs/supabase/supabase.types";
+import { useAuth } from "@/libs/providers/auth-provider";
+import { useRouter } from "next/navigation";
 
 interface Props {}
 
 const SettingsForm: React.FC<Props> = ({}) => {
+  const router = useRouter();
+  const { toast } = useToast();
+  const { user } = useAuth();
+  const titleTimerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const { state, workspaceId, dispatch } = useAppState();
+  const [permission, setPermission] = useState<string>("Private");
+  const [collaborators, setCollaborators] = useState<UserI[]>([]);
+  const [openAlertMessage, setOpenAlertMessage] = useState<boolean>(false);
+  const [workspaceDetails, setWorkspaceDetails] = useState();
+  const [uploadingProfilePic, setUploadingProfilePic] = useState<boolean>(false);
+
   return <div>SettingsForm</div>;
 };
 
