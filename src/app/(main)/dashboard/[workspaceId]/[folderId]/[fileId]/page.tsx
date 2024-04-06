ff7b07c822b2f7a234156b07ccf6bfde3bf6e7f2
zuhedshaikh95 <zuhed95@gmail.com>
2024-04-06 23:49:18 +0530

    add: QuillEditor

--- src/app/(main)/dashboard/[workspaceId]/[folderId]/[fileId]/page.tsx
+++ src/app/(main)/dashboard/[workspaceId]/[folderId]/[fileId]/page.tsx
@@ -1,8 +1,24 @@
-"use client";
-import { useParams } from "next/navigation";
+export const dynamic = "force-dynamic";
+
+import { QuillEditor } from "@/components";
+import { getFileDetails } from "@/libs/supabase/queries";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function File() {
-  const { fileId } = useParams();
-  return <div>{fileId}</div>;
+interface PageProps {
+  params: {
+    fileId: string;
+  };
+}
+
+export default async function File({ params }: PageProps) {
+  const { data, error } = await getFileDetails(params.fileId);
+
+  if (error || !data) redirect("/dashboard");
+
+  return (
+    <div className="relative">
+      <QuillEditor dirDetails={data} dirType="file" fileId={params.fileId} />
+    </div>
+  );
 }
