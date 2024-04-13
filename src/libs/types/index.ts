e5270c6a78f27e89c509cace66b10cb764dd295e
zuhedshaikh95 <zuhed95@gmail.com>
2024-04-13 23:31:48 +0530

    add: NextApiResponseServerIoT type

--- src/libs/types/index.ts
+++ src/libs/types/index.ts
@@ -1,5 +1,17 @@
+import type { NextApiResponse } from "next";
+import type { Socket, Server } from "net";
+import type { Server as SocketIoServer } from "socket.io";
+
 export type NavbarMenuItemI = {
   title: string;
   href: string;
   description: string;
 };
+
+export interface NextApiResponseServerIoT extends NextApiResponse {
+  socket: Socket & {
+    server: Server & {
+      io: SocketIoServer;
+    };
+  };
+}
