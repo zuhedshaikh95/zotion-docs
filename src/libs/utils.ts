202cc17a94d99b1709ecdd76860b731b2cec8808
zuhedshaikh95 <zuhed95@gmail.com>
2024-04-26 08:52:43 +0530

    add: formatPrice

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,6 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { PriceI } from "./supabase/supabase.types";
+import { prices } from "../../migrations/schema";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatPrice = (price: PriceI) => {
+  const priceString = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: price.currency ?? undefined,
+    minimumFractionDigits: 0,
+  }).format((price.unitAmount || 0) / 100);
+
+  return priceString;
+};
