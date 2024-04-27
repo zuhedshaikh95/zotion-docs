d85e6e6d3d0a51dc764b1d15b866a7806d742d6a
zuhedshaikh95 <zuhed95@gmail.com>
2024-04-27 22:25:28 +0530

    add: CustomException, toDateTime, getUrl, postData

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,10 +1,17 @@
-import { type ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { PriceI } from "./supabase/supabase.types";
-import { prices } from "../../migrations/schema";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export class CustomException extends Error {
+  status: number;
+  constructor(message: string, status: number) {
+    super(message);
+    this.status = status;
+  }
 }
 
 export const formatPrice = (price: PriceI) => {
@@ -15,4 +22,36 @@ export const formatPrice = (price: PriceI) => {
   }).format((price.unitAmount || 0) / 100);
 
   return priceString;
+};
+
+export const toDateTime = (seconds: number) => {
+  const dateTime = new Date("1970-01-01T00:30:00Z");
+  dateTime.setSeconds(seconds);
+
+  return dateTime;
+};
+
+export const getUrl = () => {
+  let url = process.env.NEXT_PUBLIC_SITE_URL ?? process.env.NEXT_PUBLIC_RAILWAY_URL ?? "http://localhost:3000";
+
+  url = url.startsWith("http") ? url : `https://${url}`;
+  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+
+  return url;
+};
+
+export const postData = async ({ url, data }: { url: string; data?: { price?: PriceI; origin?: string } }) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    credentials: "same-origin",
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    console.log("Error in postData", { url, data, response });
+    throw Error(response.statusText);
+  }
+
+  return response.json();
 };
