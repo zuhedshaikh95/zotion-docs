a2e0fe6a5376f506ebfefd8ce5fd0357023ff325
zuhedshaikh95 <zuhed95@gmail.com>
2024-04-27 22:20:20 +0530

    add: fetch products

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -1,15 +1,15 @@
 import { SubscriptionModalProvider } from "@/libs/providers/subscription-modal-provider";
+import { getActiveProductsWithPrice } from "@/libs/supabase/queries";
 import React from "react";
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const { data: products, error } = await getActiveProductsWithPrice();
+
+  if (error) throw new Error("Something went wrong! Try again later");
+
   return (
-    <main
-      className="
-        flex
-        overflow-hidden
-        h-screen"
-    >
-      <SubscriptionModalProvider>{children}</SubscriptionModalProvider>
+    <main className="flex overflow-hidden h-screen">
+      <SubscriptionModalProvider products={products}>{children}</SubscriptionModalProvider>
     </main>
   );
 }
