764d11ec127e9642992b09b6272076aa3e02dc39
zuhedshaikh95 <zuhed95@gmail.com>
2024-04-28 23:48:57 +0530

    add: Suspense boundary

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -1,10 +1,14 @@
 import { Container } from "@/components";
-import React from "react";
+import React, { Suspense } from "react";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export default function AuthTemplate({ children }: Props) {
-  return <Container className="h-screen pt-6 flex justify-center sm:items-center">{children}</Container>;
+  return (
+    <Suspense>
+      <Container className="h-screen pt-6 flex justify-center sm:items-center">{children}</Container>;
+    </Suspense>
+  );
 }
